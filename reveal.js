8e8eab3f501a47461295edacdb44be65cb9efa00
AndrewMalcher <andrew.malcher.r@gmail.com>
2024-12-10 13:47:21 -0400

    feat: add scroll reveal animations and improve element visibility

---
+++ reveal.js
@@ -0,0 +1,44 @@
+window.revelar = ScrollReveal({ reset: true });
+
+// topo do site
+revelar.reveal(".efeito-showcase-container", {
+	duration: 2000,
+	origin: "top",
+	distance: "120px",
+});
+revelar.reveal(".efeito-aparecer-topo", {
+	delay: 100,
+	duration: 2000,
+	origin: "top",
+	distance: "120px",
+});
+revelar.reveal(".efeito-aparecer-esquerda", {
+	delay: 100,
+	duration: 1000,
+	origin: "left",
+	distance: "120px",
+});
+revelar.reveal(".efeito-aparecer-direita", {
+	delay: 500,
+	duration: 1000,
+	origin: "right",
+	distance: "120px",
+});
+revelar.reveal(".aparecer1", {
+	delay: 500,
+	duration: 1000,
+	origin: "right",
+	distance: "120px",
+});
+revelar.reveal(".aparecer2", {
+	delay: 900,
+	duration: 1300,
+	origin: "right",
+	distance: "120px",
+});
+revelar.reveal(".aparecer3", {
+	delay: 1300,
+	duration: 1300,
+	origin: "right",
+	distance: "120px",
+});
